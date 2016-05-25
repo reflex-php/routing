7e0931c385e7fce8b297a617cb594fecc6c1150e
Mike Shellard <contact@mikeshellard.me>
2016-05-25 08:57:54 +0100

    using a different default fallout handler

--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@ export const defaultConfig = {
     /**
      * Fallout function, handles errors
      */
-    fallout: code => throw new Error(`[Router] Fallout code: ${code}`),
+    fallout: code => {
+        throw new Error(`[Router] Fallout code: ${code}`)
+    },
 
     defaultRouteKey: 'default'
 };
